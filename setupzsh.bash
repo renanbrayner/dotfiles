7d95b2af6af3110d1eb485ed25ebdd056e63a186
Renan Brayner <renanbrayner@gmail.com>
2020-10-16 18:13:00 -0300

    warning in installation

--- setupzsh.bash
+++ setupzsh.bash
@@ -3,15 +3,24 @@ echo "---- BACKUPING .oh-my-zsh AND .zshrc"
 mv ~/.oh-my-zsh ~/.oh-my-zsh.backup
 mv ~/.zshrc ~/.zshrc.backup
 
+cat << EOF
+
+     #    ####### ####### #     # ####### ### ####### #     # ### 
+    # #      #    #       ##    #    #     #  #     # ##    # ### 
+   #   #     #    #       # #   #    #     #  #     # # #   # ### 
+  #     #    #    #####   #  #  #    #     #  #     # #  #  #  #  
+  #######    #    #       #   # #    #     #  #     # #   # #     
+  #     #    #    #       #    ##    #     #  #     # #    ## ### 
+  #     #    #    ####### #     #    #    ### ####### #     # ### 
+
+CLOSE THE OTHER TERMINAL AFTER THE OH-MY-ZSH INSTALLATION TO PROCEED
+EOF
+
 echo "---- INSTALLING Oh-my-zsh IN ANOTHER TERMINAL EMULATOR"
 kitty -e sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-echo "ATENTION: Press return after Oh-my-zsh installation"
-read wait
-
 echo "---- INSTALLING POWERLEVEL9k THEME"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 
 echo "---- STOWING .zshrc"
 stow zsh
