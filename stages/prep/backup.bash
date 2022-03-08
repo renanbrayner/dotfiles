e532700acd2a057253b1ead501cb9f1948a07795
Renan Brayner <renanbrayner@gmail.com>
2022-03-08 01:50:07 -0300

    review backups

--- stages/prep/backup.bash
+++ stages/prep/backup.bash
@@ -2,39 +2,41 @@
 cat << EOF
 ---------------------------------BACKUPING OLD CONFIGS--------------------------------
 Comands used:
-  mv ~/.config/alacritty    ~/.config/alacritty.backup 
-  mv ~/.config/assets       ~/.config/assets.backup
-  mv ~/.config/coc          ~/.config/coc.backup 
-  mv ~/.config/deadd        ~/.config/deadd.backup
-  mv ~/.config/kitty        ~/.config/kitty.backup 
-  mv ~/.config/neofetch     ~/.config/neofetch.backup
-  mv ~/.config/nvim         ~/.config/nvim.backup
-  mv ~/.config/picom.conf   ~/.config/picom.conf.backup
-  mv ~/.config/tint2        ~/.config/tint2.backup
-  mv ~/.config/qutebrowser  ~/.config/qutebrowser.backup
-  mv ~/.config/ranger       ~/.config/ranger.backup
-  mv ~/.config/rofi         ~/.config/rofi.backup 
-  mv ~/.config/sxhkd        ~/.config/sxhkd.backup
-  mv ~/.icons               ~/.icons.backup
-  mv ~/.themes              ~/.themes.backup
-  mv ~/.xmonad              ~/.xmonad.backup   
+  mv ~/.config/alacritty   ~/.config/alacritty.backup
+  mv ~/.config/assets      ~/.config/assets.backup
+  mv ~/.config/calcurse    ~/.config/calcurse.backup
+  mv ~/.config/deadd       ~/.config/deadd.backup
+  mv ~/.config/kitty       ~/.config/kitty.backup
+  mv ~/.config/nvim        ~/.config/nvim.backup
+  mv ~/.config/picom.conf  ~/.config/picom.conf.backup
+  mv ~/.config/qutebrowser ~/.config/qutebrowser.backup
+  mv ~/.config/ranger      ~/.config/ranger.backup
+  mv ~/.config/rofi        ~/.config/rofi.backup
+  mv ~/.config/sxhkd       ~/.config/sxhkd.backup
+  mv ~/.xprofile           ~/.xprofile.backup
+  mv ~/.Xresources         ~/.Xresources.backup
+  mv ~/.zshrc              ~/.zshrc.backup
+  mv ~/.icons              ~/.icons.backup
+  mv ~/.themes             ~/.themes.backup
 
 Warning: if you have any file/folder named the same as any backup it will lost.
+
 EOF
 
-mv ~/.config/alacritty    ~/.config/alacritty.backup 
-mv ~/.config/assets       ~/.config/assets.backup
-mv ~/.config/coc          ~/.config/coc.backup 
-mv ~/.config/deadd        ~/.config/deadd.backup
-mv ~/.config/kitty        ~/.config/kitty.backup 
-mv ~/.config/neofetch     ~/.config/neofetch.backup
-mv ~/.config/nvim         ~/.config/nvim.backup
-mv ~/.config/picom.conf   ~/.config/picom.conf.backup
-mv ~/.config/tint2        ~/.config/tint2.backup
-mv ~/.config/qutebrowser  ~/.config/qutebrowser.backup
-mv ~/.config/ranger       ~/.config/ranger.backup
-mv ~/.config/rofi         ~/.config/rofi.backup 
-mv ~/.config/sxhkd        ~/.config/sxhkd.backup
-mv ~/.icons               ~/.icons.backup
-mv ~/.themes              ~/.themes.backup
-mv ~/.xmonad              ~/.xmonad.backup   
+mv ~/.config/alacritty   ~/.config/alacritty.backup
+mv ~/.config/assets      ~/.config/assets.backup
+mv ~/.config/calcurse    ~/.config/calcurse.backup
+mv ~/.config/deadd       ~/.config/deadd.backup
+mv ~/.config/kitty       ~/.config/kitty.backup
+mv ~/.config/nvim        ~/.config/nvim.backup
+mv ~/.config/picom.conf  ~/.config/picom.conf.backup
+mv ~/.config/qutebrowser ~/.config/qutebrowser.backup
+mv ~/.config/ranger      ~/.config/ranger.backup
+mv ~/.config/rofi        ~/.config/rofi.backup
+mv ~/.config/sxhkd       ~/.config/sxhkd.backup
+mv ~/.xprofile           ~/.xprofile.backup
+mv ~/.Xresources         ~/.Xresources.backup
+mv ~/.zshrc              ~/.zshrc.backup
+mv ~/.icons              ~/.icons.backup
+mv ~/.themes             ~/.themes.backup
+
