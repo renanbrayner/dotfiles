751bea95572f99d8ab37fac8e6d5d06ba4640798
Renan Brayner <renanbrayner@gmail.com>
2022-03-08 01:51:18 -0300

    review delete backups

--- deletebackups.bash
+++ deletebackups.bash
@@ -2,35 +2,40 @@
 cat << EOF
 -----------------------------------DELETING BACKUPS-----------------------------------
 Comands used:
-  rm -rf ~/.config/alacritty.backup 
+  rm -rf ~/.config/alacritty.backup
   rm -rf ~/.config/assets.backup
-  rm -rf ~/.config/coc.backup 
-  rm -rf ~/.config/dunstrc.backup
-  rm -rf ~/.config/i3.backup   
-  rm -rf ~/.config/neofetch.backup
+  rm -rf ~/.config/calcurse.backup
+  rm -rf ~/.config/deadd.backup
+  rm -rf ~/.config/kitty.backup
   rm -rf ~/.config/nvim.backup
   rm -rf ~/.config/picom.conf.backup
-  rm -rf ~/.config/polybar.backup
   rm -rf ~/.config/qutebrowser.backup
   rm -rf ~/.config/ranger.backup
-  rm -rf ~/.config/rofi.backup 
+  rm -rf ~/.config/rofi.backup
+  rm -rf ~/.config/sxhkd.backup
+  rm -rf ~/.xprofile.backup
+  rm -rf ~/.Xresources.backup
+  rm -rf ~/.zshrc.backup
   rm -rf ~/.icons.backup
   rm -rf ~/.themes.backup
 
+Warning: be sure you don't have any important files under the names above
+
 EOF
 
-rm -rf ~/.config/alacritty.backup 
+rm -rf ~/.config/alacritty.backup
 rm -rf ~/.config/assets.backup
-rm -rf ~/.config/coc.backup 
-rm -rf ~/.config/dunstrc.backup
-rm -rf ~/.config/i3.backup   
-rm -rf ~/.config/kitty.backup 
-rm -rf ~/.config/neofetch.backup
+rm -rf ~/.config/calcurse.backup
+rm -rf ~/.config/deadd.backup
+rm -rf ~/.config/kitty.backup
 rm -rf ~/.config/nvim.backup
 rm -rf ~/.config/picom.conf.backup
-rm -rf ~/.config/polybar.backup
 rm -rf ~/.config/qutebrowser.backup
 rm -rf ~/.config/ranger.backup
-rm -rf ~/.config/rofi.backup 
+rm -rf ~/.config/rofi.backup
+rm -rf ~/.config/sxhkd.backup
+rm -rf ~/.xprofile.backup
+rm -rf ~/.Xresources.backup
+rm -rf ~/.zshrc.backup
 rm -rf ~/.icons.backup
 rm -rf ~/.themes.backup
