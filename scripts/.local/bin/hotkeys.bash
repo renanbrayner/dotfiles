f723c81eafa33c23ac0b904a888368b6b6b6bba2
Renan Brayner <renanbrayner@gmail.com>
2022-03-22 00:09:22 -0300

    change primary terminal and browser

--- scripts/.local/bin/hotkeys.bash
+++ scripts/.local/bin/hotkeys.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-export my_primary_browser=brave
-export my_secondary_browser=firefox
-export my_primary_terminal=kitty
-export my_secondary_terminal=alacritty
+export my_primary_browser=firefox
+export my_secondary_browser=brave
+export my_primary_terminal=alacritty
+export my_secondary_terminal=kitty
 export DISPLAY=:0
 
 EDITOR=nvim
