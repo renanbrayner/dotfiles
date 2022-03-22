ad7a2bb87caa228f9f3f0aaf4a87169ce291c373
Renan Brayner <renanbrayner@gmail.com>
2022-03-22 00:08:37 -0300

    add a image preview with text to ranger

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -321,7 +321,8 @@ handle_mime() {
         ## Image
         image/*)
             ## Preview as text conversion
-            # img2txt --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4
+            img2txt --format=utf8 --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4 # changed to utf8 format
+            # chafa -c 256 -s "${PV_WIDTH}x${PV_HEIGHT}" -- "${FILE_PATH}" && exit 4
             exiftool "${FILE_PATH}" && exit 5
             exit 1;;
 
