89611723da229f95e3a99c964b9cf4bdb021b8ed
Renan Brayner <renanbrayner@gmail.com>
2022-04-01 03:39:41 -0300

    change brightness notification

--- scripts/.local/bin/brightness.bash
+++ scripts/.local/bin/brightness.bash
@@ -1,18 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$1" == "--up" ]; then
+if [ "$1" = "--up" ]; then
     xbacklight -inc +10%
 fi
 
-if [ "$1" == "--down" ]; then
+if [ "$1" = "--down" ]; then
     xbacklight -inc -10%
 fi
 
 BRIGHTNESS=$(xbacklight -get)
 
-notify-send.sh "Brightness" "${BRIGHTNESS%.*}/100" \
-    --replace-file=/tmp/brightnessnotification \
+notify-send.sh "Brightness" \
     -a "" \
-    --hint string:image-path:video-display \
     --hint int:has-percentage:${BRIGHTNESS%.*} \
+    --hint string:image-path:video-display \
     --hint int:transient:1 \
+    --replace-file=/tmp/brightnessnoti \
+
+# WITH NUMBER
+# notify-send.sh "Brightness" "${BRIGHTNESS%.*}/100" \
+#     --replace-file=/tmp/brightnessnotification \
+#     -a "" \
+#     --hint string:image-path:video-display \
+#     --hint int:has-percentage:${BRIGHTNESS%.*} \
+#     --hint int:transient:1 \
